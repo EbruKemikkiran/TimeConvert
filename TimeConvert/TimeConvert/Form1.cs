89b3d76f7692095a8909dc087593d932fbf97783
Ebru Kemikkiran <104608342+EbruKemikkiran@users.noreply.github.com>
2022-10-04 21:14:39 +0100

    Add files via upload

---
+++ TimeConvert/TimeConvert/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Value1;
+            int Time, Minute;
+
+
+            Value1 = Convert.ToInt32(textBox1.Text);
+
+           Time = Value1/ 60;
+            label2.Text = Time.ToString();
+
+            Minute = Value1 % 60;
+            label4.Text = Minute.ToString();
+
+        }
+    }
+}
